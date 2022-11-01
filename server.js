bab190f11a7fda380bbf63c9c99da6ed1df75708
Yulissa1633 <cielo.camacho@ucr.ac.cr>
2022-10-31 20:39:58 -0600

    updates to deploy to heroku 10

--- server.js
+++ server.js
@@ -16,8 +16,3 @@ res.sendFile(path.join(__dirname+'/src/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.set("port", port);
-
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
