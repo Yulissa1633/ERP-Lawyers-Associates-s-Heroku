15e8ac038d09dc2e3c2d1792afd9e20914d7a8fd
Yulissa1633 <cielo.camacho@ucr.ac.cr>
2022-10-31 20:55:46 -0600

    updates to deploy to heroku 11

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-//Install express server
+
 const express = require('express');
-const path = require('path');
-const http = require('http');
-
 const app = express();
-const port = http.createServer(process.env.PORT || 3000);
+const path = require('path');
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ERP-Lawyers-Associates-s-Heroku'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/src/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/src/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.set("port", port);
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
