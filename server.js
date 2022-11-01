cc169ce3e132e8a44e06f5ad737ce8b5d92b8be7
Yulissa1633 <cielo.camacho@ucr.ac.cr>
2022-10-31 20:11:27 -0600

    updates to deploy to heroku 6

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || '8080';
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ERP-Lawyers-Associates-s-Heroku'));
@@ -13,4 +14,5 @@ res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+app.set("port", PORT);
