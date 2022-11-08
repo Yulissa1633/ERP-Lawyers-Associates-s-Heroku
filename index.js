fd5f83364231151380540eb654f3cefc3e33a5c3
Yulissa1633 <cielo.camacho@ucr.ac.cr>
2022-11-08 10:03:48 -0600

    express api access

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.set('env', NODE_ENV);
 app.use(logger('tiny'));
 app.use(bodyParser.json());
 app.use('/', routes);
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+    });
 app.use((req, res, next) => {
   const err = new Error(`${req.method} ${req.url} Not Found`);
   err.status = 404;
