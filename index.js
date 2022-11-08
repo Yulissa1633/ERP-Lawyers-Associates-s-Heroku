44c92b708d3fc8dd7f98220389b5b9731d85d00c
Yulissa1633 <cielo.camacho@ucr.ac.cr>
2022-11-08 10:27:09 -0600

    express api access 3

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use('/', routes);
 app.use(function (req, res, next) {
     //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://erplawyerswebapp-ki7v3jvnd-yulissa1633.vercel.app/list-collaborator");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
